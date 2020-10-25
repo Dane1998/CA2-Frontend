84c118c4e238fb9701961db9bd1acd68f45488ef
Daniel <daniel.poulsen@live.dk>
2020-10-25 10:39:23 +0100

    frontend

--- src/index.js
+++ src/index.js
@@ -1,49 +1,108 @@
 import "./style.css"
 import "bootstrap/dist/css/bootstrap.css"
-import "./jokeFacade"
-import jokeFacade from "./jokeFacade"
+
 
 document.getElementById("all-content").style.display = "block"
 
-/* 
-  Add your JavaScript for all exercises Below or in separate js-files, which you must the import above
-*/
-
-/* JS For Exercise-1 below */
-
-
-/* JS For Exercise-2 below */
-
-
-
-/* JS For Exercise-3 below */
-
-
-/* 
-Do NOT focus on the code below, UNLESS you want to use this code for something different than
-the Period2-week2-day3 Exercises
-*/
-
+// Er metoden som viser den side som der skal vises ud fra ID //
 function hideAllShowOne(idToShow) {
-  document.getElementById("about_html").style = "display:none"
-  document.getElementById("ex1_html").style = "display:none"
-  document.getElementById("ex2_html").style = "display:none"
-  document.getElementById("ex3_html").style = "display:none"
+  document.getElementById("home_html").style = "display:none"
+  document.getElementById("groupMembers_html").style = "display:none"
+  document.getElementById("allEndPoints_html").style = "display:none"
   document.getElementById(idToShow).style = "display:block"
 }
+// Metoden slutter her //
 
+
+// Er metoden som tager id på den side der skal vises og kalder den forrige //
 function menuItemClicked(evt) {
   const id = evt.target.id;
   switch (id) {
-    case "ex1": hideAllShowOne("ex1_html"); break
-    case "ex2": hideAllShowOne("ex2_html"); break
-    case "ex3": hideAllShowOne("ex3_html"); break
-    default: hideAllShowOne("about_html"); break
+    case "home": hideAllShowOne("home_html"); break
+    case "groupMembers": hideAllShowOne("groupMembers_html"); break
+    case "allEndPoints": hideAllShowOne("allEndPoints_html"); break
+    default: hideAllShowOne("home_html"); break
   }
   evt.preventDefault();
 }
 document.getElementById("menu").onclick = menuItemClicked;
-hideAllShowOne("about_html");
+hideAllShowOne("home_html");
+
+// Metoden slutter her //
 
 
 
+// Starten af search og map metoden //
+  const baseUrl = "https://jsonplaceholder.typicode.com/users/" // Det endpoint som der bliver hentet fra
+  const resultBox = document.getElementById('result'); 
+
+
+  const mapMembersToHtml = (members) => {
+    const html = members.map(({id, name, username, email, address, phone, company}) => (
+      `<tr>
+        <td>${id}</td>
+        <td>${name}</td>
+        <td>${username}</td>
+        <td>${email}</td>
+        <td>${address.street}</td>
+        <td>${address.city}</td>
+        <td>${address.zipcode}</td>
+        <td>${phone}</td>
+        <td>${company.name}</td>
+      </tr>`
+    )).join('');
+
+    resultBox.innerHTML = 
+            ` <table border= "2"> 
+                  <thead> 
+                      <tr> 
+                          <td>Person-ID</td> 
+                          <td>Name</td> 
+                          <td>Username</td> 
+                          <td>Email</td> 
+                          <td>Address</td> 
+                          <td>City</td> 
+                          <td>Zipcode</td> 
+                          <td>Phone number</td>
+                          <td>Hobby</td>
+                      </tr> 
+                  </thead> 
+                  <tbody> 
+                      ${html} 
+                  </tbody> 
+                </table> `; 
+  }         
+  
+  const searchInMembers = (query) => {
+    if (query.length === 0) {
+      return
+    }
+//Matcher basis værdi med den nye værdi, dette gør expansion langt nemmere.
+    const matchValues = [
+      (element) => element.name,
+      (element) => element.phone,
+      (element) => element.address.street,
+      (element) => element.address.city,
+      (element) => element.email
+    ]
+
+    const queryValue = query.toLowerCase()
+    fetch(baseUrl)
+      .then(response => response.json())
+      .then(elements => {
+        const filtered = elements.filter(element => {
+          for (let matchValue of matchValues) {
+            const comparableValue = matchValue(element).toLowerCase()
+            if (queryValue === comparableValue || comparableValue.includes(queryValue)) {
+              return true
+            }
+          }
+          return false
+        })
+      mapMembersToHtml(filtered)
+      })
+  } 
+  document.getElementById('findButton').addEventListener('click', function(event) {
+      event.preventDefault();
+      searchInMembers(document.getElementById('searchField').value)
+  });
